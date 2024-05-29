123dec7a1039677ed0a401d85e54a81204443de1
Guillermocr221 <guillermo.calle@unmsm.edu.pe>
2024-05-28 21:58:48 -0500

    Herencia Agregada  a los Modelos
    Evitar replicacion de codigo.

--- Desarrollo/TreeSolution/Codigo/Backend/includes/app.php
+++ Desarrollo/TreeSolution/Codigo/Backend/includes/app.php
@@ -1,5 +1,6 @@
 <?php 
 
+
 use Model\Producto;
 require __DIR__ . '/../vendor/autoload.php';
 
--- Desarrollo/TreeSolution/Codigo/Backend/models/TipoPago.php
+++ Desarrollo/TreeSolution/Codigo/Backend/models/TipoPago.php
@@ -2,52 +2,8 @@
 
 namespace Model;
 
-class TipoPago{
+class TipoPago extends ActiveRecord{
 
-    //Base de datos
-    protected static $db;
-
-    //Definir conexion a la DB
-    public static function setDB($database){
-        self::$db = $database;
-    }
-
-    //Listar todos los productos
-    public static function all(){
-        $query = "SELECT * FROM TipoPago";
-
-        $resultado = self::consultarSQL($query);
-
-        return $resultado;
-
-    }
-    public static function consultarSQL($query){
-        //Consultar la base de datos
-        $resultado = self::$db->query($query);
-        
-        //Iterar los resultados
-        $array = [];
-        while($registro = $resultado->fetch_assoc()){
-            $array[] = self::crearObjeto($registro); 
-        }
-
-        //liberar memoria
-        $resultado->free();
-
-        //retornar los resultados
-        return $array;
-    }
+    protected static $tabla = '';
     
-    // Funcion para convertir un arreglo asociativo en Objeto
-    protected static function crearObjeto($registro){
-        $objeto = new self; // crea nuevos objetos de la clase actual
-
-        foreach($registro as $key => $value){
-            // if(property_exists($objeto, $key)){
-                $objeto->$key = $value;
-            // }
-        }
-        // debuguear($objeto);
-        return $objeto;
-    }
 }    
